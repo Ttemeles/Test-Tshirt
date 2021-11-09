9bc77e37e3d8105b82ecc60bc8f4d9ec14fcd257
Ttemeles <91227969+Ttemeles@users.noreply.github.com>
2021-11-09 06:47:17 -0500

    Update targetShooter.java

--- src/main/java/frc/robot/Commands/targetShooter.java
+++ src/main/java/frc/robot/Commands/targetShooter.java
@@ -1,5 +1,29 @@
 package frc.robot.Commands;
 
 public class targetShooter {
+    public double horizOffset, vertOffset, angleOffset;
+    public boolean targetExist;
+    public targetShooter() {
+        requires(Robot.shooter);
     
 }
+    
+    @Override
+    protected void execute()
+    { 
+        double leftStickval = Robot.oi.getControllerValue(1);
+        double rightStickVal = Robot.oi.getControllerValue(4);
+        
+        double vertPower = verOffset/8;
+        
+        if(OI.controllerLeftBumper()) {
+            vertPower = -.5;
+        }
+        else if (OI.controllerRightBumper()) {
+            vertPower= .5;
+        }
+        else if (Math.abs(vertOffset) > 0 && targetExist) {
+            vertPower = vertOFfset/8 * -1;
+        }
+        
+        
