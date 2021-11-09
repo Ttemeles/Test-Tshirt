ee86829565c51314802f90e66228a9b99dedb0cf
Ttemeles <91227969+Ttemeles@users.noreply.github.com>
2021-11-09 07:51:42 -0500

    Update targetShooter.java

--- src/main/java/frc/robot/Commands/targetShooter.java
+++ src/main/java/frc/robot/Commands/targetShooter.java
@@ -16,14 +16,51 @@ public class targetShooter {
         
         double vertPower = verOffset/8;
         
+        
+       //reducing values, less power to shoot
+      
         if(OI.controllerLeftBumper()) {
             vertPower = -.5;
         }
         else if (OI.controllerRightBumper()) {
             vertPower= .5;
         }
-        else if (Math.abs(vertOffset) > 0 && targetExist) {
+        else if (Math.abs(vertOffset) > 0 && targetExist) {      //
             vertPower = vertOFfset/8 * -1;
         }
         
+        else {
+            vertPower = 0; 
+        }
+        
+        double latPower = 0;
+        double horzPower = horizOffset/8;
+        double miniPower = .3;
+        double turnleft = horizOffset > 0 ? 1:-1;
+        minPower = minPower * turnleft;
+        horzPower = Robot.drive.limit(horzPower, .75, -.75);
+        
+        if(Math.abs(leftStickVal) > .15 || Math.abs(rightStickVal) > .15)
+            
+        {
+            horzPower = rightStickVal;
+            latPower = leftStickVal;
+        }
+        
+        else if(Math.abs(leftStickVal) > .15 && Math.abs(horizOffset) > 0 && targetExist)
+        {
+            horzPower = horzPower + minPower;
+            latPower = leftStickVal;
+        }
+        
+        else if(Math.abs(horizOffset) > 0 &&targetExist) {
+            horzPower = horzPower + minPower;
+        }
+        else{
+            horzPower=0;
+            latpower = 0;
+        }
+        
+        double VP = .75, HP = 1;
+           
         
