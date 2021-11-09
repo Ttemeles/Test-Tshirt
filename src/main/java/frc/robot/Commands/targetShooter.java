a653ac44d5ce2510e664ec8e3a20dff74318a943
Ttemeles <91227969+Ttemeles@users.noreply.github.com>
2021-11-09 08:23:09 -0500

    Update targetShooter.java

--- src/main/java/frc/robot/Commands/targetShooter.java
+++ src/main/java/frc/robot/Commands/targetShooter.java
@@ -3,6 +3,9 @@ package frc.robot.Commands;
 public class targetShooter {
     public double horizOffset, vertOffset, angleOffset;
     public boolean targetExist;
+  
+    
+    
     public targetShooter() {
         requires(Robot.shooter);
     
@@ -10,22 +13,20 @@ public class targetShooter {
     
     @Override
     protected void execute()
-    { 
+    {      
         double leftStickval = Robot.oi.getControllerValue(1);
         double rightStickVal = Robot.oi.getControllerValue(4);
-        
-        double vertPower = verOffset/8;
-        
-        
-       //reducing values, less power to shoot
+                                                //vertOffset means reducing range of fire vertically
+        double vertPower = verOffset/8;     
+     
       
-        if(OI.controllerLeftBumper()) {
+        if(OI.controllerLeftBumper()) {     //allows the T-shirt cannnon to fire at people up high with accuracy
             vertPower = -.5;
         }
         else if (OI.controllerRightBumper()) {
             vertPower= .5;
         }
-        else if (Math.abs(vertOffset) > 0 && targetExist) {      //
+        else if (Math.abs(vertOffset) > 0 && targetExist) {     //a backup loop for code
             vertPower = vertOFfset/8 * -1;
         }
         
@@ -34,7 +35,7 @@ public class targetShooter {
         }
         
         double latPower = 0;
-        double horzPower = horizOffset/8;
+        double horzPower = horizOffset/8;e
         double miniPower = .3;
         double turnleft = horizOffset > 0 ? 1:-1;
         minPower = minPower * turnleft;
@@ -42,18 +43,20 @@ public class targetShooter {
         
         if(Math.abs(leftStickVal) > .15 || Math.abs(rightStickVal) > .15)
             
-        {
-            horzPower = rightStickVal;
-            latPower = leftStickVal;
+        {       
+            horzPower = rightStickVal;      //relying on contorller values
+                                            //rightStick controls horizontally, allows to shoot at one person or close to
+            latPower = leftStickVal;            //no vertical distance needed
         }
         
         else if(Math.abs(leftStickVal) > .15 && Math.abs(horizOffset) > 0 && targetExist)
         {
-            horzPower = horzPower + minPower;
-            latPower = leftStickVal;
+            horzPower = horzPower + minPower                    // if just lefStick is pressed and this value is true, do this
+            latPower = leftStickVal;                            //for special cirucumstances
         }
         
-        else if(Math.abs(horizOffset) > 0 &&targetExist) {
+        else if(Math.abs(horizOffset) > 0 &&targetExist) {      //same thing, if controller is not pressed, but it senses someone, then still fire
+            
             horzPower = horzPower + minPower;
         }
         else{
@@ -61,6 +64,23 @@ public class targetShooter {
             latpower = 0;
         }
         
-        double VP = .75, HP = 1;
+        double VP = .75;
+        double HP = 1;
+        vertPower = vertPower * VP;
+        horPower = horzPower * HP;
+        Shooter.moveAerm(vertPower);
+        Robot.drive.arcadeDrive(latPower, horzPower);
+        
+        SmartDashboard.putNumber("Horizontal Drive Power", horzPower);
+        SmartDashboard.pitNumber("vertical Arm Power", vertPower);
+        
+        @Overide
+        protected boolean isFinished() {
+            returnMath.abs(horizOffset)<2 &&Math.abs(vertOffset) >=2 || Robot.oi.getControllerValue(0) >.2;
+            
+        }}
+        
+        
+        
            
         
